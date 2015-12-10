d8ec19431a0aff00fe0f6bbffdb693d00f04bcaf
Matt Crummey <mcrummey@validusa.com>
2015-12-10 21:11:29 +0000

    adding scopes to tests

--- test/couchConfigureTest.js
+++ test/couchConfigureTest.js
@@ -78,7 +78,6 @@ describe("The couch-configure library ", function () {
             })
             .then((body) => {
                 body.toString("ascii").should.equal("test");
-
                 scope.done();
                 done();
             })
@@ -143,7 +142,8 @@ describe("The couch-configure library ", function () {
 
     it("merge should overwrite data", function (done) {
         // Here we are mocking a couch authentication
-        nock("http://test/")
+        let scope2;
+        let scope = nock("http://test/")
             .post("/_session")
             .reply(200, {
                 ok: true,
@@ -154,14 +154,13 @@ describe("The couch-configure library ", function () {
                 "set-cookie": "Yummo"
             });
         couchdb.initialize("http://test", "tester", "pass", "test").then(function (response) {
-            nock("http://test/")
+            scope2 = nock("http://test/")
                 .get("/test/9")
                 .reply(200, {
                     _id: "9",
                     testKey1: "originalVal",
                     testKey2: "originalVal"
-                });
-            nock("http://test/")
+                })
                 // We can use Nock to match the expected JSON body.
                 .post("/test", {
                     _id: "9",
@@ -180,6 +179,8 @@ describe("The couch-configure library ", function () {
             });
         }).then(function (body) {
             body.ok.should.equal(true);
+            scope.done();
+            scope2.done();
             done();
         }).catch(function (reason) {
             done(reason);
@@ -188,7 +189,7 @@ describe("The couch-configure library ", function () {
     });
     it("replace should get the right rev", function (done) {
         // Here we are mocking a couch authentication
-        nock("http://test/")
+        let scope = nock("http://test/")
             .post("/_session")
             .reply(200, {
                 ok: true,
@@ -206,17 +207,15 @@ describe("The couch-configure library ", function () {
             })
             .reply(200, {
                 ok: true
+            })
+            .head("/test/9")
+            .reply(200, {}, {
+                etag: "99-ce54ca73ee9cfaec22610765aa6f04d5"
             });
         couchdb.initialize("http://test", "tester", "pass")
             .then(function (response) {
                 couchdb.use("test");
                 console.log("Login Response " + JSON.stringify(response));
-                nock("http://test/")
-                    .head("/test/9")
-                    .reply(200, {}, {
-                        etag: "99-ce54ca73ee9cfaec22610765aa6f04d5"
-                    });
-
                 return couchdb.replace({
                     _id: "9",
                     testKey1: "updatedVal",
@@ -225,6 +224,7 @@ describe("The couch-configure library ", function () {
             })
             .then(function (body) {
                 body.ok.should.equal(true);
+                scope.done();
                 done();
             })
             .catch(function (reason) {
@@ -233,14 +233,15 @@ describe("The couch-configure library ", function () {
             });
     });
     it("addAdmin should create an admin user without authenticating ", function (done) {
-        nock("http://test/")
+        let scope = nock("http://test/")
             .put("/_config/admins/testadmin")
             .reply(200, {
                 ok: true
             });
         couchdb.addAdmin("http://test", "testadmin", "testPass").then(function (body) {
             body.ok.should.equal(true);
-            // array 0 is body
+            // array 0 is bodya
+            scope.done();
             done();
         }).catch(function (reason) {
             done(reason);
@@ -250,7 +251,7 @@ describe("The couch-configure library ", function () {
 
     it("addAdmin should create an admin user without authenticating ", function (done) {
         // Here we are mocking a couch authentication
-        nock("http://test/")
+        let scope = nock("http://test/")
             .post("/_session")
             .reply(200, {
                 ok: true,
@@ -261,7 +262,7 @@ describe("The couch-configure library ", function () {
                 "set-cookie": "Yummo"
             });
 
-        nock("http://test/", {
+        let scope2 = nock("http://test/", {
             reqheaders: {
                 Cookie: "Yummo"
             }
@@ -275,6 +276,8 @@ describe("The couch-configure library ", function () {
         }).then(function (body) {
             body.ok.should.equal(true);
             // array 0 is body
+            scope.done();
+            scope2.done();
             done();
         }).catch(function (reason) {
             done(reason);
@@ -282,9 +285,9 @@ describe("The couch-configure library ", function () {
         });
     });
     it("We should update the cookie when it changes", function (done) {
+        let scope2;
         // Here we are mocking a couch authentication
-        nock("http://test/")
-
+        let scope = nock("http://test/")
             .post("/_session")
             .reply(200, {
                 ok: true,
@@ -312,7 +315,7 @@ describe("The couch-configure library ", function () {
                 });
             })
             .then(function (body) {
-                nock("http://test/")
+                scope2 = nock("http://test/")
                     .matchHeader("cookie", (val) => {
                         console.log("!!!!Cookie Value " + val);
                         return (val.search("Yammo") !== -1);
@@ -329,6 +332,8 @@ describe("The couch-configure library ", function () {
             })
             .then(function (body) {
                 body.ok.should.equal(true);
+                scope.done();
+                scope2.done();
                 done();
             })
             .catch(function (reason) {
